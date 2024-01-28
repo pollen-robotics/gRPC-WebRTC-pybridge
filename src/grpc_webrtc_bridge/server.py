c50f3b31ca98ba0c5c64afdb76b1f0cb39288ce8
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-01-28 14:52:10 +0100

    enhancement #19: working version. draft

--- src/grpc_webrtc_bridge/server.py
+++ src/grpc_webrtc_bridge/server.py
@@ -1,12 +1,14 @@
 import argparse
 import asyncio
 import logging
+import os
 import sys
 import time
 from threading import Semaphore
 
-import aiortc
-from gst_signalling import GstSession, GstSignallingProducer
+import gi
+from gst_signalling import GstSignallingProducer
+from gst_signalling.gst_abstract_role import GstSession
 from reachy2_sdk_api.webrtc_bridge_pb2 import (
     AnyCommands,
     Connect,
@@ -16,6 +18,10 @@ from reachy2_sdk_api.webrtc_bridge_pb2 import (
 )
 
 from .grpc_client import GRPCClient
+
+gi.require_version("Gst", "1.0")
+
+from gi.repository import GLib, Gst
 
 last_freq_counter = 0
 last_freq_update = time.time()
@@ -43,6 +49,14 @@ class GRPCWebRTCBridge:
 
             grpc_client = GRPCClient(args.grpc_host, args.grpc_port)
 
+            data_channel = pc.emit("create-data-channel", "service", None)
+            if data_channel:
+                data_channel.connect("on-open", self.on_open_service_channel)
+                data_channel.connect("on-message-data", self.on_data_service_channel, grpc_client, pc)
+            else:
+                self.logger.error("Failed to create data channel")
+
+            """
             service_channel = pc.createDataChannel("service")
 
             @service_channel.on("message")  # type: ignore[misc]
@@ -53,6 +67,24 @@ class GRPCWebRTCBridge:
                 response = await self.handle_service_request(request, grpc_client, pc)
 
                 service_channel.send(response.SerializeToString())
+            """
+
+    def on_open_service_channel(self, channel: Gst.Element) -> None:
+        self.logger.info("channel service opened")
+
+    def on_data_service_channel(self, data_channel, message: GLib.Bytes, grpc_client: GRPCClient, pc) -> None:  # type: ignore[no-untyped-def]
+        self.logger.info(f"Message from DataChannel: {message}")
+        request = ServiceRequest()
+        request.ParseFromString(message.get_data())
+
+        # response = await self.handle_service_request(request, grpc_client, pc)
+        # loop = asyncio.new_event_loop()
+        # response = loop.run_until_complete(self.handle_service_request(request, grpc_client, pc))
+        asyncio.run_coroutine_threadsafe(
+            self.handle_service_request(request, data_channel, grpc_client, pc), self.producer._asyncloop
+        )
+
+        # data_channel.send_string(response.SerializeToString())
 
     async def serve4ever(self) -> None:
         await self.producer.serve4ever()
@@ -61,12 +93,7 @@ class GRPCWebRTCBridge:
         await self.producer.close()
 
     # Handle service request
-    async def handle_service_request(
-        self,
-        request: ServiceRequest,
-        grpc_client: GRPCClient,
-        pc: aiortc.RTCPeerConnection,
-    ) -> ServiceResponse:
+    async def handle_service_request(self, request: ServiceRequest, data_channel, grpc_client: GRPCClient, pc) -> None:  # type: ignore[no-untyped-def]
         self.logger.info(f"Received service request message: {request}")
 
         if request.HasField("get_reachy"):
@@ -79,7 +106,12 @@ class GRPCWebRTCBridge:
             resp = await self.handle_disconnect_request()
 
         self.logger.info(f"Sending service response message: {resp}")
-        return resp
+        # return resp
+
+        # data_channel.send_string(resp.SerializeToString())
+        byte_data = resp.SerializeToString()
+        gbyte_data = GLib.Bytes.new(byte_data)
+        data_channel.send_data(gbyte_data)
 
     async def handle_get_reachy_request(self, grpc_client: GRPCClient) -> ServiceResponse:
         reachy = await grpc_client.get_reachy()
@@ -95,18 +127,95 @@ class GRPCWebRTCBridge:
 
         return resp
 
-    async def handle_connect_request(  # noqa: C901
-        self,
-        request: Connect,
-        grpc_client: GRPCClient,
-        pc: aiortc.RTCPeerConnection,
-    ) -> ServiceResponse:
+    def on_open_state_channel(self, channel: Gst.Element, request: Connect, grpc_client: GRPCClient) -> None:
+        self.logger.info("channel state opened")
+
+        async def send_joint_state() -> None:
+            async for state in grpc_client.get_reachy_state(
+                request.reachy_id,
+                request.update_frequency,
+            ):
+                # channel.send(state.SerializeToString())
+                byte_data = state.SerializeToString()
+                gbyte_data = GLib.Bytes.new(byte_data)
+                channel.send_data(gbyte_data)
+
+        asyncio.run_coroutine_threadsafe(send_joint_state(), self.producer._asyncloop)
+
+    def on_reachy_command_datachannel_message(self, data_channel, message: GLib.Bytes, grpc_client: GRPCClient) -> None:  # type: ignore[no-untyped-def]
+        global last_freq_counter
+        global last_freq_update
+        global last_drop_counter
+        global freq_rates
+        global drop_rates
+        global init
+
+        commands = AnyCommands()
+        commands.ParseFromString(message.get_data())
+
+        if not commands.commands:
+            self.logger.info("No command or incorrect message received {message}")
+            return
+
+        # take lock
+        if self.smart_lock.acquire(blocking=False):
+            last_freq_counter += 1
+            # await grpc_client.handle_commands(commands)
+            # self.producer._asyncloop.run_until_complete(grpc_client.handle_commands(commands))
+            future = asyncio.run_coroutine_threadsafe(grpc_client.handle_commands(commands), self.producer._asyncloop)
+            try:
+                result = future.result(timeout=0.5)
+            except TimeoutError:
+                self.logger.warning("The coroutine took too long, cancelling the task...")
+                future.cancel()
+            except Exception as exc:
+                self.logger.error(f"The coroutine raised an exception: {exc!r}")
+
+            now = time.time()
+            if now - last_freq_update > 1:
+                current_freq_rate = int(last_freq_counter / (now - last_freq_update))
+                current_drop_rate = int(last_drop_counter / (now - last_freq_update))
+
+                self.logger.info(f"Freq {current_freq_rate} Hz\tDrop {current_drop_rate} Hz")
+
+                if init:
+                    freq_rates.append(current_freq_rate)
+                    drop_rates.append(current_drop_rate)
+                    if len(freq_rates) > 10000:
+                        freq_rates.pop(0)
+                        drop_rates.pop(0)
+                    mean_freq_rate = sum(freq_rates) / len(freq_rates)
+                    mean_drop_rate = sum(drop_rates) / len(drop_rates)
+                    self.logger.info(f"[MEAN] Freq {mean_freq_rate} Hz\tDrop {mean_drop_rate} Hz")
+                else:
+                    init = True
+                # Calculate mean values
+
+                last_freq_counter = 0
+                last_drop_counter = 0
+                last_freq_update = now
+
+            self.smart_lock.release()
+        else:
+            # self.logger.info("Nevermind, I'll send the next one")
+            last_drop_counter += 1
+
+    async def handle_connect_request(self, request: Connect, grpc_client: GRPCClient, pc) -> ServiceResponse:  # type: ignore[no-untyped-def]
         # Create state data channel and start sending state
         if request.update_frequency >= 1000:
             max_packet_lifetime = 1
         else:
             max_packet_lifetime = (int)(1000 // request.update_frequency)
 
+        channel_options = Gst.Structure.new_empty("application/x-data-channel")
+        channel_options.set_value("max-packet-lifetime", max_packet_lifetime)
+        data_channel_state = pc.emit("create-data-channel", f"reachy_state_{request.reachy_id.id}", channel_options)
+        if data_channel_state:
+            data_channel_state.connect("on-open", self.on_open_state_channel)
+        else:
+            self.logger.error("Failed to create data channel state")
+
+        """
         reachy_state_datachannel = pc.createDataChannel(
             f"reachy_state_{request.reachy_id.id}",
             maxPacketLifeTime=max_packet_lifetime,
@@ -125,9 +234,20 @@ class GRPCWebRTCBridge:
                     logging.info("Data channel closed.")
 
             asyncio.ensure_future(send_joint_state())
+        """
+
+        channel_options = Gst.Structure.new_empty("application/x-data-channel")
+        channel_options.set_value("max-packet-lifetime", 20)
+        reachy_command_datachannel = pc.emit("create-data-channel", f"reachy_command_{request.reachy_id.id}", channel_options)
+        if reachy_command_datachannel:
+            # reachy_command_datachannel.connect("on-open", self.on_open_command_channel)
+            reachy_command_datachannel.connect("on-message-data", self.on_reachy_command_datachannel_message, grpc_client)
+        else:
+            self.logger.error("Failed to create data channel command")
 
         # Create command data channel and start handling commands
         # assuming data sent by Unity in the FixedUpdate loop. frequency 50Hz / ev 20ms
+        """
         reachy_command_datachannel = pc.createDataChannel(f"reachy_command_{request.reachy_id.id}", maxPacketLifeTime=20)
 
         @reachy_command_datachannel.on("message")  # type: ignore[misc]
@@ -181,7 +301,7 @@ class GRPCWebRTCBridge:
             else:
                 # self.logger.info("Nevermind, I'll send the next one")
                 last_drop_counter += 1
-
+            """
         return ServiceResponse()
 
     async def handle_disconnect_request(self) -> ServiceResponse:
@@ -232,7 +352,8 @@ def main() -> int:  # noqa: C901
     args = parser.parse_args()
 
     if args.verbose:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
+        os.environ["GST_DEBUG"] = "3"
 
     bridge = GRPCWebRTCBridge(args)
 
